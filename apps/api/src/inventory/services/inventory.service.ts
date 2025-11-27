97c63e3ccee7cd47603653694821d0e7487c6a60
qutubkothari <kothariqutub@gmail.com>
2025-11-27 21:29:54 +0530

    Add non-null assertions for env variables

--- apps/api/src/inventory/services/inventory.service.ts
+++ apps/api/src/inventory/services/inventory.service.ts
@@ -8,8 +8,8 @@ export class InventoryService {
 
   constructor() {
     this.supabase = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_KEY!
     );
   }
 
